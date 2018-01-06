b16be73155bf3f3bcb65537edfa6c7991afa7691
poshuk <poshuk2009@gmail.com>
2018-01-06 02:15:13 +0200

    The method translate is completed.
    Signed-off-by: poshuk <poshuk2009@gmail.com>

--- Controller.java
+++ Controller.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Controller {
     private View view;
     private static final Map<Character, String> firstEl = new HashMap<>();
-    private static final Map<String, String> lastEl = new HashMap<>();
+    private static final Map<Character, String> lastEl = new HashMap<>();
 
     public Controller(View view){
         this.view = view;
@@ -31,38 +31,38 @@ public class Controller {
         firstEl.put('ю', "yu");
         firstEl.put('я', "ya");
 
-        lastEl.put("а", "a");
-        lastEl.put("б", "b");
-        lastEl.put("в", "v");
-        lastEl.put("г", "h");
-        lastEl.put("ґ", "g");
-        lastEl.put("д", "d");
-        lastEl.put("е", "e");
-        lastEl.put("є", "ie");
-        lastEl.put("ж", "zh");
-        lastEl.put("з", "z");
-        lastEl.put("и", "y");
-        lastEl.put("і", "i");
-        lastEl.put("ї", "i");
-        lastEl.put("й", "i");
-        lastEl.put("к", "k");
-        lastEl.put("л", "l");
-        lastEl.put("м", "m");
-        lastEl.put("н", "n");
-        lastEl.put("о", "o");
-        lastEl.put("п", "p");
-        lastEl.put("р", "r");
-        lastEl.put("с", "s");
-        lastEl.put("т", "t");
-        lastEl.put("у", "u");
-        lastEl.put("ф", "f");
-        lastEl.put("х", "kh");
-        lastEl.put("ц", "ts");
-        lastEl.put("ч", "ch");
-        lastEl.put("ш", "sh");
-        lastEl.put("щ", "shch");
-        lastEl.put("ю", "iu");
-        lastEl.put("я", "ia");
+        lastEl.put('а', "a");
+        lastEl.put('б', "b");
+        lastEl.put('в', "v");
+        lastEl.put('г', "h");
+        lastEl.put('ґ', "g");
+        lastEl.put('д', "d");
+        lastEl.put('е', "e");
+        lastEl.put('є', "ie");
+        lastEl.put('ж', "zh");
+        lastEl.put('з', "z");
+        lastEl.put('и', "y");
+        lastEl.put('і', "i");
+        lastEl.put('ї', "i");
+        lastEl.put('й', "i");
+        lastEl.put('к', "k");
+        lastEl.put('л', "l");
+        lastEl.put('м', "m");
+        lastEl.put('н', "n");
+        lastEl.put('о', "o");
+        lastEl.put('п', "p");
+        lastEl.put('р', "r");
+        lastEl.put('с', "s");
+        lastEl.put('т', "t");
+        lastEl.put('у', "u");
+        lastEl.put('ф', "f");
+        lastEl.put('х', "kh");
+        lastEl.put('ц', "ts");
+        lastEl.put('ч', "ch");
+        lastEl.put('ш', "sh");
+        lastEl.put('щ', "shch");
+        lastEl.put('ю', "iu");
+        lastEl.put('я', "ia");
     }
 
     public void translate(){
@@ -88,15 +88,50 @@ public class Controller {
                 String l = lastName.toLowerCase().replaceAll("\\p{Punct}*[ь]*", "");
                 String zghFirstName = f.replaceAll("зг", "zgh");
                 String zghLastName = l.replaceAll("зг", "zgh");
+                int i = 0;
+
                 for (Character c : zghFirstName.toCharArray()) {
-                    int i = 0;
-                    if (i==0 && firstEl.containsKey(c)){
+                    if (i == 0 && firstEl.containsKey(c)) {
                         sbFirstName.append(firstEl.get(c));
                         i++;
+                    } else if (lastEl.containsKey(c)) {
+                        i++;
+                        sbFirstName.append(lastEl.get(c));
+                    } else if (c == 'z'){
+                        StringBuilder check = new StringBuilder();
+                        check.append(c);
+                        check.append(zghFirstName.charAt(i+1));
+                        check.append(zghFirstName.charAt(i+2));
+                        if (check.toString().equals("zgh")){
+                            sbFirstName.append(check);
+                            i +=2;
+                        }
+                        i++;
                     }
-                    i++;
                 }
-                //view.getResultField().setText(zghFirstName + "." + zghLastName);
+
+                i=0;
+                for (Character c : zghLastName.toCharArray()) {
+                    if (i == 0 && firstEl.containsKey(c)) {
+                        sbLastName.append(firstEl.get(c));
+                        i++;
+                    } else if (lastEl.containsKey(c)) {
+                        i++;
+                        sbLastName.append(lastEl.get(c));
+                    } else if (c == 'z'){
+                        StringBuilder check1 = new StringBuilder();
+                        check1.append(c);
+                        check1.append(zghLastName.charAt(i+1));
+                        check1.append(zghLastName.charAt(i+2));
+                        if (check1.toString().equals("zgh")){
+                            sbLastName.append(check1);
+                            i +=2;
+                        }
+                        i++;
+                    }
+                }
+
+                view.getResultField().setText(sbFirstName.toString()+"."+sbLastName.toString());
 
             }
 
